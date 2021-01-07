162bded1b4a48f26e555d13d9819b083207cc4ae
Bharath Kumar Reddy <bharath.reddy@wibmo.com>
2021-01-07 10:24:08 +0530

    git setup

---
+++ Neo/Neo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Neo
+//
+//  Created by Bharath Kumar Reddy on 07/01/21.
+//  Copyright © 2021 Wibmo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("test")
+    }
+
+
+}
+
